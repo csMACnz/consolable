950c23cd4403a2367066946c4d4ed931f37509fa
Mark Clearwater <csmacnz@csmac.co.nz>
2016-07-06 21:05:48 +0100

    :sake: PSake time!

---
+++ default.ps1
@@ -0,0 +1,30 @@
+properties {
+    $base_dir = . resolve-path .\
+}
+
+# Probably will just build, decide later
+task default
+
+task build {
+    cd src/csMACnz.Consolable
+    dotnet build
+    cd ../csMACnz.Consolable.Tests
+    dotnet build
+}
+
+task test {
+    cd src/csMACnz.Consolable.Tests
+    dotnet test
+}
+
+task pack {
+    $package_dir = "$base_dir\pack"
+    if (Test-Path $package_dir) {
+        Remove-Item $package_dir -r
+    }
+    mkdir $package_dir
+
+    cd src/csMACnz.Consolable
+    dotnet pack -c Release -o $package_dir
+}
+
