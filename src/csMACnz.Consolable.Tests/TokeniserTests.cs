9bd82f6529fd8cd0d4930c29fd2b3f8597e52aa3
Mark Clearwater <csmacnz@csmac.co.nz>
2016-07-06 20:30:53 +0100

    First! (test)

---
+++ src/csMACnz.Consolable.Tests/TokeniserTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace csMACnz.Consolable.Tests
+{
+    public class TokeniserTests
+    {
+        [Fact]
+        public void Tokeniser_EmptyInput_EmptyOutput() 
+        {
+            var input = new string[0];
+            var sut = new Tokeniser();
+
+            var result = sut.GetTokens(input);
+
+            Assert.NotNull(result);
+            Assert.Empty(result);
+        }
+    }
+}
