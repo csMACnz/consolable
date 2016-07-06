ba08054271902ef13c7061bea024f224ba8bab24
Mark Clearwater <csmacnz@csmac.co.nz>
2016-07-06 20:09:14 +0100

    added test project

---
+++ src/csMACnz.Consolable.Tests/Tests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace csMACnz.Consolable.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1() 
+        {
+            var tokeniser = new csMACnz.Consolable.Tokeniser();
+            Assert.True(true);
+        }
+    }
+}
