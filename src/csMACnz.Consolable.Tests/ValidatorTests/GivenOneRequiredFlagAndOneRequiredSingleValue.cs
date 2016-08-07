f7100b5a410ee53591e507fc16c62300217a60aa
Mark Clearwater <csmacnz@csmac.co.nz>
2016-08-07 18:05:28 +0100

    finished renamin ValidatorTests

---
+++ src/csMACnz.Consolable.Tests/ValidatorTests/GivenOneRequiredFlagAndOneRequiredSingleValue.cs
@@ -0,0 +1,59 @@
+using Xunit;
+
+namespace csMACnz.Consolable.Tests.ValidatorTests
+{
+    public class GivenOneRequiredFlagAndOneRequiredSingleValue
+    {
+        private IRule[] _rules;
+        public GivenOneRequiredFlagAndOneRequiredSingleValue()
+        {
+            _rules = new IRule[] {
+                new RequiredArgument('a', "alpha", ArgumentMode.Flag),
+                new RequiredArgument('b', "bravo", ArgumentMode.SingleValue)
+            };
+        }
+
+        [Fact]
+        public void ValidateArguments_MissingSingleValueTokenThenValidArgument_OneError()
+        {
+            var input = new[] { new Token(TokenType.Arg, "b", "-b", 1), new Token(TokenType.Arg, "a", "-a", 1) };
+
+            var result = Validator.ValidateArguments(_rules, input);
+
+            Assert.Collection(
+                result,
+                e =>
+                {
+                    Assert.NotNull(e);
+                    Assert.Equal(ErrorType.MissingValue, e.Type);
+                    Assert.Equal("b", e.ErrorToken.Value);
+                    Assert.Equal(1, e.ErrorToken.RawIndex);
+                    Assert.Equal("-b", e.ErrorToken.Raw);
+                    Assert.Equal("b", e.Argument);
+                }
+            );
+        }
+
+        [Fact]
+        public void ValidateArguments_ValidArgumentThenMissingSingleValueToken_OneError()
+        {
+            var input = new[] { new Token(TokenType.Arg, "a", "-a", 1), new Token(TokenType.Arg, "b", "-b", 1) };
+
+            var result = Validator.ValidateArguments(_rules, input);
+
+            Assert.Collection(
+                result,
+                e =>
+                {
+                    Assert.NotNull(e);
+                    Assert.Equal(ErrorType.MissingValue, e.Type);
+                    Assert.Equal("b", e.ErrorToken.Value);
+                    Assert.Equal(1, e.ErrorToken.RawIndex);
+                    Assert.Equal("-b", e.ErrorToken.Raw);
+                    Assert.Equal("b", e.Argument);
+                }
+            );
+        }
+
+    }
+}
